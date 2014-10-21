08af1d826f66a1d2722166048de2b347baada0f4
Alex Hawkins <alexhawkins.me@gmail.com>
2014-10-20 18:12:44 -0700

    Basic data visualizations

--- js/script.js
+++ js/script.js
@@ -1,7 +1,51 @@
 'use strict';
 
-var dataset = [1, 3, 4, 23, 45, 29];
+var dataset = [25, 7, 5, 26, 11, 8, 25, 14, 23, 19, 14, 11, 22, 29, 11, 13, 12, 17, 18, 10, 24, 18, 25, 9, 3];
 
+/** BASIC BAR GRAPH SCALED x 4 */
+d3.select('section.set1').selectAll('div')
+    .data(dataset)
+    .enter()
+    .append('div')
+    .attr('class', 'bar')
+    .style('height', function(d) {
+        return d * 4 + 'px';
+    });
+
+
+d3.select('section.set2').selectAll('div')
+    .data(dataset.sort(function(x, y) {
+        return x - y;
+    }))
+    .enter()
+    .append('div')
+    .attr('class', 'bar')
+    .style('height', function(d) {
+        return d * 4 + 'px';
+    })
+    .style('background-color', function(d) {
+        var hue = Math.floor(Math.random() * 153);
+        return 'rgb(153,' + hue + ',' + hue + ')';
+    });
+
+var newData = []
+var newData = dataset.map(function(el) {
+    return el + Math.floor(Math.random() * 30);
+})
+d3.select('section.set3').selectAll('div')
+    .data(newData)
+    .enter()
+    .append('div')
+    .attr('class', 'bar')
+    .style('height', function(d) {
+        return d * 4 + 'px';
+    })
+    .style('background-color', function(d) {
+        var hue = Math.floor(Math.random() * 254);
+        return 'rgb(254,' + hue + ',' + hue + ')';
+    });
+
+/*
 d3.select('body').selectAll('p')
     .data(dataset)
     .enter()
@@ -9,6 +53,22 @@ d3.select('body').selectAll('p')
     .text(function(data) {
         return data;
     })
-    .style('color', function(d){
-      return d > 24 ? 'blue' : 'red';
-    });
+    .style('color', function(d) {
+        return d > 24 ? 'blue' : 'red';
+    });*/
+var w = 500, h = 50;
+
+var svg = d3.select('section.svg1')
+    .append('svg')
+    .attr('width', w)
+    .attr('height', h)
+
+var dataset = [5,10,15,20,25];
+var circles = svg.selectAll('circle')
+    .data(dataset)
+    .enter()
+    .append('circle')
+        .attr('cx', function(d, i) { return (i * 50) + 25; } )
+        .attr('cy', h / 2)
+        .attr('r', function(d) { return d; } )
+        .style('fill', '#000');
